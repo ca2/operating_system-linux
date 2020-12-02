5535a2a33df952f7262d663af91e6b8a3dd5f683
Camilo Sasuke Tbs_ <camilosasuketbs@ca2.email>
2020-12-01 21:04:16 -0300

    [Manjaro20.1.2/Linux] CMake and CLion as Build/Development Alternative
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    cmgriffing streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    -- hi5 contribution...!!
    
    at Manjaro 20.1.2 Dual Boot at eddie machine through Anydesk from bilbo running Windows 10 Pro!!

--- app/app.cpp
+++ app/app.cpp
@@ -122,7 +122,7 @@
 //
 //   }
 //
-////   pcommand->m_nCmdShow              = display_normal;
+////   pcommand->m_nCmdShow              = e_display_normal;
 //
 //   psystem->startup_command(pcommand);
 //
